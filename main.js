597498a8dfbf4628bd3b7e07b9fab86a99f7e5ec
Haleigh Smith <haleighbsmiths@gmail.com>
2023-08-31 16:09:06 -0400

    Thank god that is over

---
+++ main.js
@@ -0,0 +1,50 @@
+if (1+1 > 5) {
+console.log("True fact!");
+} else {
+    console.log("LIES!");
+}
+if (4*5 <= 20) {
+    console.log("True fact!");
+} else {
+    console.log("LIES!");
+}
+if (6-2 >= 0) {
+    console.log("True fact!");
+} else {
+    console.log("LIES!");
+}
+let likesDogs = true
+if (likesDogs === true) {
+    console.log("You're a dog person!");
+} else {
+    console.log("You're a cat person!");
+}
+let person = {
+    name: "Allee",
+    preferredLanguage: "French"
+}
+if (person.preferredLanguage === "English") {
+    console.log(`Hello, ${person.name}!`);
+}
+if (person.preferredLanguge === "Spanish") {
+    console.log(`Hola, ${person.name}`);
+}
+if (person.preferredLanguage === "French") {
+    console.log(`Bonjour, ${person.name}`);
+} 
+let grade = 80
+if (grade >= 0 && grade <= 69) {
+    console.log ("You got an F");
+}
+if (grade >= 70 && grade <= 76) {
+    console.log ("You got an D");
+}
+if (grade >= 77 && grade <= 84) {
+    console.log ("You got an C");
+}
+if (grade >= 84 && grade <= 92) {
+    console.log ("You got an B");
+}
+if (grade >= 93 && grade <= 100) {
+    console.log ("You got an A");
+}
